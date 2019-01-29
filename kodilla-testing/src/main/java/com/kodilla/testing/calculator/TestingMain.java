2529b0f54379b8db46c6e36eafc22d8891dffb1e
matirori <mateuszr600@gmail.com>
2019-01-29 20:19:49 +0100

    Pierwszy test jednostkowy

--- kodilla-testing/src/main/java/com/kodilla/testing/calculator/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/TestingMain.java
@@ -1,4 +1,23 @@
 package com.kodilla.testing.calculator;
 
 public class TestingMain {
+    public static void main(String[] args){
+        Calculator number = new Calculator(5,2);
+
+        int result = number.add();
+
+        if(result == 5+2){
+            System.out.println("Everything working properly");
+        }else{
+            System.out.println("Something is wrong");
+        }
+
+        result = number.substract();
+
+        if (result == 5-2){
+            System.out.println("Everything working properly");
+        }else{
+            System.out.println("Something is wrong");
+        }
+    }
 }
