855fd8d7e95159dce091fdcf958bff9c6cf5a96f
matirori <mateuszr600@gmail.com>
2019-02-20 00:10:08 +0100

    zadanie 6.6 - part1

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/library/Book.java
@@ -0,0 +1,54 @@
+package com.kodilla.testing.library;
+
+public class Book {
+    String title;
+    String author;
+    int publicationYear;
+
+    public Book(String title, String author, int publicationYear) {
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", publicationYear=" + publicationYear +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Book)) return false;
+
+        Book book = (Book) o;
+
+        if (getPublicationYear() != book.getPublicationYear()) return false;
+        if (getTitle() != null ? !getTitle().equals(book.getTitle()) : book.getTitle() != null) return false;
+        return getAuthor() != null ? getAuthor().equals(book.getAuthor()) : book.getAuthor() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getTitle() != null ? getTitle().hashCode() : 0;
+        result = 31 * result + (getAuthor() != null ? getAuthor().hashCode() : 0);
+        result = 31 * result + getPublicationYear();
+        return result;
+    }
+}
