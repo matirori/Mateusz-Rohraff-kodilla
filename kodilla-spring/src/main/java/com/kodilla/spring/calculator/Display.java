db9d34a2a900e871262e3e069a40ed314d4d9b20
matirori <mateuszr600@gmail.com>
2019-07-19 03:44:17 +0200

    zadanie 14.2 - part1

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/calculator/Display.java
@@ -0,0 +1,11 @@
+package com.kodilla.spring.calculator;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Display {
+
+    public double displayValue(double val) {
+        return val;
+    }
+}
---
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -0,0 +1,25 @@
+package com.kodilla.spring.calculator;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CalculatorTestSuite {
+    @Test
+    public void testCalculations() {
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Display display = context.getBean(Display.class);
+        //When
+        display.displayValue(2);
+        //Then
+
+    }
+}
