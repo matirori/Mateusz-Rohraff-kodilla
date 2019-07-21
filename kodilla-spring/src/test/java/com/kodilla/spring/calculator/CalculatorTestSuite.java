1ad16bd50bfd5235f7018ff9ab8c9964c4712fd9
matirori <mateuszr600@gmail.com>
2019-07-22 01:16:51 +0200

    zadanie 14.2 - part2

--- kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -16,10 +16,16 @@ public class CalculatorTestSuite {
         //Given
         ApplicationContext context =
                 new AnnotationConfigApplicationContext("com.kodilla.spring");
-        Display display = context.getBean(Display.class);
+        Calculator calculator = context.getBean(Calculator.class);
         //When
-        display.displayValue(2);
+        double sumResult = calculator.add(2,1);
+        double subResult = calculator.sub(2,1);
+        double mulResult = calculator.mul(2,1);
+        double divResult = calculator.div(2,1);
         //Then
-
+        Assert.assertEquals(new Display().displayValue(3), sumResult, 0);
+        Assert.assertEquals(new Display().displayValue(1), subResult, 0);
+        Assert.assertEquals(new Display().displayValue(2), mulResult, 0);
+        Assert.assertEquals(new Display().displayValue(2), divResult, 0);
     }
 }
