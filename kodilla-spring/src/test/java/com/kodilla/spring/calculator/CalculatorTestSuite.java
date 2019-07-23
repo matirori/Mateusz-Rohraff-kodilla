7fabca36618521968566b0ae46947478cc42afd3
matirori <mateuszr600@gmail.com>
2019-07-23 19:01:01 +0200

    zadanie 14.2 - part3

--- kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -23,9 +23,9 @@ public class CalculatorTestSuite {
         double mulResult = calculator.mul(2,1);
         double divResult = calculator.div(2,1);
         //Then
-        Assert.assertEquals(new Display().displayValue(3), sumResult, 0);
-        Assert.assertEquals(new Display().displayValue(1), subResult, 0);
-        Assert.assertEquals(new Display().displayValue(2), mulResult, 0);
-        Assert.assertEquals(new Display().displayValue(2), divResult, 0);
+        Assert.assertEquals(3, sumResult, 0);
+        Assert.assertEquals(1, subResult, 0);
+        Assert.assertEquals(2, mulResult, 0);
+        Assert.assertEquals(2, divResult, 0);
     }
 }
