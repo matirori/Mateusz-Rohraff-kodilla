54a88f93354ccc39976a3b8f47b6a981a28323fd
matirori <mateuszr600@gmail.com>
2019-08-26 00:12:52 +0200

    zadanie 17.4 - part1

---
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/manytomany/dao/CompanyDao.java
@@ -0,0 +1,16 @@
+package com.kodilla.hibernate.manytomany.manytomany.dao;
+
+import com.kodilla.hibernate.manytomany.Company;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Transactional
+@Repository
+public interface CompanyDao extends CrudRepository<Company, Integer> {
+    @Query
+    String retrieveCompaniesByFirstThreeLetters(@Param("COMPANY_CODE") String companyCode);
+}
---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/dao/CompanyDaoTestSuite.java
@@ -0,0 +1,108 @@
+package com.kodilla.hibernate.manytomany.dao;
+
+import com.kodilla.hibernate.manytomany.Company;
+import com.kodilla.hibernate.manytomany.Employee;
+import com.kodilla.hibernate.manytomany.manytomany.dao.CompanyDao;
+import com.kodilla.hibernate.manytomany.manytomany.dao.EmployeeDao;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CompanyDaoTestSuite {
+    @Autowired
+    CompanyDao companyDao;
+
+    @Autowired
+    EmployeeDao employeeDao;
+
+    @Test
+    public void testSaveManyToMany(){
+        //Given
+        Employee johnSmith = new Employee("John", "Smith");
+        Employee stephanieClarckson = new Employee("Stephanie", "Clarckson");
+        Employee lindaKovalsky = new Employee("Linda", "Kovalsky");
+
+        Company softwareMachine = new Company("Software Machine");
+        Company dataMaesters = new Company("Data Maesters");
+        Company greyMatter = new Company("Grey Matter");
+
+        softwareMachine.getEmployees().add(johnSmith);
+        dataMaesters.getEmployees().add(stephanieClarckson);
+        dataMaesters.getEmployees().add(lindaKovalsky);
+        greyMatter.getEmployees().add(johnSmith);
+        greyMatter.getEmployees().add(lindaKovalsky);
+
+        johnSmith.getCompanies().add(softwareMachine);
+        johnSmith.getCompanies().add(greyMatter);
+        stephanieClarckson.getCompanies().add(dataMaesters);
+        lindaKovalsky.getCompanies().add(dataMaesters);
+        lindaKovalsky.getCompanies().add(greyMatter);
+
+        //When
+        companyDao.save(softwareMachine);
+        int softwareMachineId = softwareMachine.getId();
+        companyDao.save(dataMaesters);
+        int dataMaestersId = dataMaesters.getId();
+        companyDao.save(greyMatter);
+        int greyMatterId = greyMatter.getId();
+
+        //Then
+        Assert.assertNotEquals(0, softwareMachineId);
+        Assert.assertNotEquals(0, dataMaestersId);
+        Assert.assertNotEquals(0, greyMatterId);
+
+        //CleanUp
+        try {
+            companyDao.deleteById(softwareMachineId);
+            companyDao.deleteById(dataMaestersId);
+            companyDao.deleteById(greyMatterId);
+        } catch (Exception e) {
+        }
+    }
+
+    @Test
+    public void testRetrievingCompanyByCompanyCode() {
+        //Given
+        Company softwareMachine = new Company("Software Machine");
+
+        companyDao.save(softwareMachine);
+        int softwareMachineId = softwareMachine.getId();
+        //When
+
+        String findSof = companyDao.retrieveCompaniesByFirstThreeLetters("Sof");
+        //Then
+
+        try {
+            Assert.assertEquals(softwareMachine.getName(), findSof);
+        } finally {
+            //CleanUp
+            companyDao.deleteById(softwareMachineId);
+        }
+    }
+
+    @Test
+    public void TestRetrievingEmployeeByName() {
+        //Given
+        Employee johnSmith = new Employee("Matthew", "Smith");
+
+        employeeDao.save(johnSmith);
+        //When
+
+        String john = employeeDao.retrievePeopleByName("Smith");
+        //Then
+        try {
+            Assert.assertEquals(johnSmith.getLastname(), john);
+        } finally {
+            //CleanUp
+            employeeDao.delete(johnSmith);
+        }
+
+    }
+}
